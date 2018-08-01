c7c8305245eba77dc4f659eef2edc74df073c153
Gary Reynolds <gary@touch.asn.au>
2018-08-02 07:38:51 +1000

    WIP

--- tests/runtests.sh
+++ tests/runtests.sh
@@ -1,16 +1,3 @@
 #!/bin/sh
 set -e
-export PYTHONPATH="./"
-export DJANGO_SETTINGS_MODULE='settings'
-
-if [ `which coverage` ] ; then
-    export COVERAGE='coverage run'
-else
-    export COVERAGE='python'
-fi
-
-$COVERAGE -m django test --traceback --settings=$DJANGO_SETTINGS_MODULE --verbosity 2 --pythonpath="../" "$@"
-
-if [ `which coverage` ] ; then
-    coverage report
-fi
+TOXENV="py$(python -c 'import sys; print("".join([str(v) for v in sys.version_info[:2]]))')-dj${DJANGO:-111}" tox
